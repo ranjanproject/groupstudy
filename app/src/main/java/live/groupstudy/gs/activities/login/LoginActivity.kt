3f232f3b46bcec6d7242de420d75ebec6ad43b4c
ranjanproject <pratikranjaniitg101@gmail.com>
2023-10-03 02:10:37 +0530

    phone number screen added in login activity

--- app/src/main/java/live/groupstudy/gs/activities/login/LoginActivity.kt
+++ app/src/main/java/live/groupstudy/gs/activities/login/LoginActivity.kt
@@ -5,32 +5,39 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
+import live.groupstudy.gs.activities.ui.GetPhoneNumberScreen
 import live.groupstudy.gs.ui.theme.GroupStudyTheme
+import live.groupstudy.gs.ui.theme.ScreenGradient
 
 class LoginActivity : ComponentActivity() {
 
-    companion object{
-        fun start(context: Context){
-          context.startActivity(Intent(context, LoginActivity::class.java))
+    companion object {
+        fun start(context: Context) {
+            context.startActivity(Intent(context, LoginActivity::class.java))
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GroupStudyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Brush.linearGradient(ScreenGradient)),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LoginActivityScreen()
                 }
             }
         }
@@ -38,17 +45,14 @@ class LoginActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LoginActivityScreen() {
+    Column(Modifier.background(Brush.linearGradient(ScreenGradient))) {
+        GetPhoneNumberScreen()
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    GroupStudyTheme {
-        Greeting("Android")
-    }
+fun LoginActivityScreenPreview() {
+    LoginActivityScreen()
 }
--- app/src/main/java/live/groupstudy/gs/activities/ui/LoginGetPhoneNumberScreen.kt
+++ app/src/main/java/live/groupstudy/gs/activities/ui/LoginGetPhoneNumberScreen.kt
@@ -1,10 +1,15 @@
 package live.groupstudy.gs.activities.ui
 
 import android.widget.EditText
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -14,7 +19,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import live.groupstudy.gs.R
@@ -32,14 +39,18 @@ fun GetPhoneNumberScreen(modifier: Modifier = Modifier){
             mutableStateOf("")
         }
         Text(text = stringResource(id = R.string.sign_in_or_create_an_account), textModifier)
-
-
+        Spacer(modifier = Modifier.height(20.dp))
 
         TextField(value = number,
-            onValueChange = {number = it},
+            onValueChange = {newNum ->if(newNum.length<=10){number = newNum}},
             modifier = textModifier,
-            placeholder = { Text(text = stringResource(id = R.string.phone_no))})
+            placeholder = { Text(text = stringResource(id = R.string.phone_no))},
+            leadingIcon = {Image(painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                contentDescription = "",  Modifier.size(30.dp))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
 
+        Spacer(modifier = Modifier.height(40.dp))
         GradientButton(text = stringResource(id = R.string.continue_btn))
     }
 }
