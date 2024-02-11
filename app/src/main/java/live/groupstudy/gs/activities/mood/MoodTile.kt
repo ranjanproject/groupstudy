3a8616c2e6d94f2f186abaa7a5f740c2c44d5660
Jiya Saraf <sarafjiya77@gmail.com>
2024-02-11 23:46:37 +0530

    I have added 5 mood tiles

--- app/src/main/java/live/groupstudy/gs/activities/mood/MoodTile.kt
+++ app/src/main/java/live/groupstudy/gs/activities/mood/MoodTile.kt
@@ -6,14 +6,20 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,22 +29,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import live.groupstudy.gs.R
-import live.groupstudy.gs.ui.theme.ScreenGradient
 
 
 @Composable
-fun MoodTile(modifier: Modifier = Modifier) {
+fun MoodTile(moodTileItem: MoodTileItem  ,  modifier: Modifier = Modifier) {
     var isSelected by remember {
-        mutableStateOf(false)
+        mutableStateOf(false
+        )
     }
-
     Column(
         modifier = modifier
             .height(dimensionResource(id = R.dimen.dimen_120))
@@ -63,122 +68,64 @@ fun MoodTile(modifier: Modifier = Modifier) {
             }
 
     ) {
-        Row {
 
-                Column {
+        Image(
+            painter = painterResource(id = moodTileItem.imageId),
+            contentDescription = null,
+            modifier = Modifier
+                .width(152.dp)
+                .height(86.dp)
+        )
+        Column(
+            Modifier
+                .padding(dimensionResource(id = R.dimen.dimen_10))
+                .fillMaxSize()
+        ) {
+            Text(
+                text = moodTileItem.title,
+                style = MaterialTheme.typography.bodySmall
+            )
 
-                    Image(
-                        painter = painterResource(id = R.drawable.silentbuddha),
-                        contentDescription = "It is an image of Buddha.",
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(dimensionResource(id = R.dimen.dimen_85))
+        }
+    }
+}
+
+            @Preview(showBackground = true)
+            @Composable
+            fun MoodTilePreview() {
+                val moodItemList = listOf<MoodTileItem>(
+                    MoodTileItem(R.drawable.silentbuddha, "Silent Buddha"),
+                    MoodTileItem(R.drawable.passionatelover, "passionate lover"),
+                    MoodTileItem(R.drawable.studypanda, "Study panda"),
+                    MoodTileItem(R.drawable.chanakya, "Chanakya"),
+                    MoodTileItem(R.drawable.ghost, "ghost"),
+
                     )
-                    Column(
-                        Modifier
-                            .padding(dimensionResource(id = R.dimen.dimen_10))
-                            .fillMaxSize()
-                    ) {
-                        Text(
-                            text = "Silent Buddha",
-                            style = MaterialTheme.typography.bodySmall
-                        )
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    contentPadding = PaddingValues(80.dp)
+
+                ) {
+                    items(moodItemList) { it ->
+                        MoodTile(it)
+                        Spacer(modifier = Modifier.height(150.dp));
+
+
                     }
                 }
-            Column {
-
-                Image(
-                    painter = painterResource(id = R.drawable.passionatelover),
-                    contentDescription = "It is an image of Bow and Arrow",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(dimensionResource(id = R.dimen.dimen_85))
-                )
-                Column(
-                    Modifier
-                        .padding(dimensionResource(id = R.dimen.dimen_10))
-                        .fillMaxSize()
-                ) {
-                    Text(
-                        text = "Passionate Lover",
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                }
             }
-        }
-        Row {
-            Column {
+//                @Composable
+//                fun MoodTileItemCell(item: MoodTileItem)
+//                {
+//                    Box(modifier = Modifier
+//                        .fillMaxWidth()
+//                        .aspectRatio(1f)
+//                        .padding(8.dp))
+//                    {
+//                        Spacer(modifier = Modifier.width(8.dp))
+//                    }
+//                }
 
-                Image(
-                    painter = painterResource(id = R.drawable.studypanda),
-                    contentDescription = "It is an image of Panda.",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(dimensionResource(id = R.dimen.dimen_85))
-                )
-                Column(
-                    Modifier
-                        .padding(dimensionResource(id = R.dimen.dimen_10))
-                        .fillMaxSize()
-                ) {
-                    Text(
-                        text = "Study Panda",
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                }
-            }
-            Column {
 
-                Image(
-                    painter = painterResource(id = R.drawable.chanakya),
-                    contentDescription = "It is an image of arrow hitting the target.",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(dimensionResource(id = R.dimen.dimen_85))
-                )
-                Column(
-                    Modifier
-                        .padding(dimensionResource(id = R.dimen.dimen_10))
-                        .fillMaxSize()
-                ) {
-                    Text(
-                        text = "Chanakya",
-                        style = MaterialTheme.typography.bodySmall
-                    )
-                }
-            }
-        }
-        Column {
 
-            Image(
-                painter = painterResource(id = R.drawable.ghost),
-                contentDescription = "It is an image of Ghost.",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(dimensionResource(id = R.dimen.dimen_85))
-            )
-            Column(
-                Modifier
-                    .padding(dimensionResource(id = R.dimen.dimen_10))
-                    .fillMaxSize()
-            ) {
-                Text(
-                    text = "Ghost",
-                    style = MaterialTheme.typography.bodySmall
-                )
-            }
-        }
-    }
-}
-@Preview(showBackground = true)
-@Composable
-fun MoodTilePreview() {
-    Box(
-        Modifier
-            .background(Brush.linearGradient(ScreenGradient))
-            .fillMaxSize()
-    ) {
-        MoodTile()
-    }
 
-}
