92af89e10248523f0bff1b9e7b835d013d32e9bc
ANKESH KUMAWAT <kumawatankesh1216@gmail.com>
2024-02-11 17:14:30 +0530

    done

--- app/src/main/java/live/groupstudy/gs/activities/mood/MoodActivity.kt
+++ app/src/main/java/live/groupstudy/gs/activities/mood/MoodActivity.kt
@@ -22,7 +22,7 @@ class MoodActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Hello Ankesh")
+                    Greeting("Android")
                 }
             }
         }
