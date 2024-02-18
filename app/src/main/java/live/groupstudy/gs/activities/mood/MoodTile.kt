32b0ed9997f07b7c07dd5bc9cf8779d0e4ea69a1
ANKESH KUMAWAT <kumawatankesh1216@gmail.com>
2024-02-18 10:33:36 +0530

    Add Moodtile and create text bar

--- app/src/main/java/live/groupstudy/gs/activities/mood/MoodTile.kt
+++ app/src/main/java/live/groupstudy/gs/activities/mood/MoodTile.kt
@@ -9,32 +9,43 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import live.groupstudy.gs.R
@@ -103,22 +114,71 @@ fun MoodTile(moodTileItem: MoodTileItem,modifier: Modifier = Modifier) {
 @Composable
 fun MoodTilePreview() {
     val moodItemList = listOf(
-        MoodTileItem(R.drawable.silentbuddha,  "Silent Buddha"),
-        MoodTileItem(R.drawable.passionatelover,  "Passionate Lover"),
-        MoodTileItem(R.drawable.studypanda,  "Study Panda"),
-        MoodTileItem(R.drawable.chanakya,  "Chanakya"),
-        MoodTileItem(R.drawable.ghost,  "Ghost")
+        MoodTileItem(R.drawable.silentbuddha, "Silent Buddha"),
+        MoodTileItem(R.drawable.passionatelover, "Passionate Lover"),
+        MoodTileItem(R.drawable.studypanda, "Study Panda"),
+        MoodTileItem(R.drawable.chanakya, "Chanakya"),
+        MoodTileItem(R.drawable.ghost, "Ghost")
     )
-    LazyVerticalGrid(columns= GridCells. Fixed(2),
-    contentPadding = PaddingValues(40.dp),
-        verticalArrangement = Arrangement.spacedBy(20.dp),
-        horizontalArrangement = Arrangement.spacedBy(20.dp)
+    var name by remember { mutableStateOf("") }
+    var showDetails by remember { mutableStateOf(false) }
 
+    Column(
+        modifier = Modifier.fillMaxSize()
     ) {
-        items(moodItemList) { it->
-            MoodTile(it)
-
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(40.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            horizontalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            items(moodItemList) { it ->
+                MoodTile(it)
+            }
         }
+        OutlinedTextField(
+            value = name,
+            onValueChange = { newName -> name = newName },
+            label = { Text("Your name") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 30.dp)
+        )
+        Spacer(modifier = Modifier.height(80.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(50.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Spacer(modifier = Modifier.width(10.dp))
+            Column(verticalArrangement = Arrangement.Center) {
+                Image(
+                    painter = painterResource(id = R.drawable.female),
+                    contentDescription = "Female logo",
+                    modifier = Modifier.size(50.dp)
+                )
+                Text("Female", modifier = Modifier.align(Alignment.CenterHorizontally))
+            }
 
+            Column(verticalArrangement = Arrangement.Center) {
+                Image(
+                    painter = painterResource(id = R.drawable.male),
+                    contentDescription = "Male logo",
+                    modifier = Modifier.size(50.dp)
+                )
+                Text("Male", modifier = Modifier.align(Alignment.CenterHorizontally))
+            }
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = { },
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(Color(0xFFFFA501)),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ) {
+            Text("Study Library")
+        }
     }
 }
