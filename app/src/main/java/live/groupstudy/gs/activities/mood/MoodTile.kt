888aad8df999ae15b774cc6d194925e2b32f268e
ANKESH KUMAWAT <kumawatankesh1216@gmail.com>
2024-03-15 20:25:31 +0530

    Add logo code

--- app/src/main/java/live/groupstudy/gs/activities/mood/MoodTile.kt
+++ app/src/main/java/live/groupstudy/gs/activities/mood/MoodTile.kt
@@ -1,94 +1,53 @@
 package live.groupstudy.gs.activities.mood
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import live.groupstudy.gs.R
-import live.groupstudy.gs.ui.theme.ScreenGradient
-
 
 @Composable
-fun MoodTile(modifier: Modifier = Modifier) {
-    var isSelected by remember {
-        mutableStateOf(false)
-    }
-
+fun GenderLogoAndText(
+    @DrawableRes resourceId: Int,
+    gender: String,
+    onClick: () -> Unit
+) {
     Column(
-        modifier = modifier
-            .height(dimensionResource(id = R.dimen.dimen_120))
-            .width(dimensionResource(id = R.dimen.dimen_150))
-            .background(
-                color = Color.White
-            ).clip(shape = RoundedCornerShape(
-                dimensionResource(id = R.dimen.dimen_10)
-            ))
-            .border(
-                width = 1.dp,
-                color = if (isSelected) Color.Red else Color.Gray,
-                shape = RoundedCornerShape(
-                    dimensionResource(id = R.dimen.dimen_10)
-                )
-            ).clickable {
-               isSelected = !isSelected
-            }
-
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
         Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(dimensionResource(id = R.dimen.dimen_85))
+            painter = painterResource(id = resourceId),
+            contentDescription = "logo",
+            modifier = Modifier.size(50.dp)
         )
-        Column(
-            Modifier
-                .padding(dimensionResource(id = R.dimen.dimen_10))
-                .fillMaxSize()
-        ) {
-            Text(
-                text = "Silent Buddha",
-                style = MaterialTheme.typography.bodySmall
-            )
-        }
-
+        Text(
+            text = gender,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MoodTilePreview() {
-    Box(
-        Modifier
-            .background(Brush.linearGradient(ScreenGradient))
-            .fillMaxSize()
-    ) {
-        MoodTile()
+fun GenderLogoAndTextPreview() {
+    Column {
+        GenderLogoAndText(
+            resourceId = R.drawable.female,
+            gender = stringResource(id = R.string.female),
+            onClick = {}
+        )
+        GenderLogoAndText(
+            resourceId = R.drawable.male,
+            gender = stringResource(id = R.string.male),
+            onClick = {}
+        )
     }
-
 }
